690f247fc85d6710dd89cb64f323a955c4a0cf11
athanoid <a.vourvopoulos@gmail.com>
2020-02-26 17:27:51 +0000

    main menu

---
+++ Assets/Scripts/MenuScripts.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuScripts : MonoBehaviour {
+
+	public Button graz;
+	public Button ntrainm;
+	public Button ntrainf;
+
+	public Button nonlinem;
+	public Button nonlinef;
+
+	// Use this for initialization
+	void Start () {
+
+		Button grazbtn = graz.GetComponent<Button>();
+		grazbtn.onClick.AddListener(delegate() { GoToLevel("graz_training"); });
+
+		Button ntrainmbtn = ntrainm.GetComponent<Button>();
+		ntrainmbtn.onClick.AddListener(delegate() { GoToLevel("boat_training"); });
+
+		Button ntrainfbtn = ntrainf.GetComponent<Button>();
+		ntrainfbtn.onClick.AddListener(delegate() { GoToLevel("boat_training_female"); });
+
+		Button nonlinembtn = nonlinem.GetComponent<Button>();
+		ntrainmbtn.onClick.AddListener(delegate() { GoToLevel("boat_online"); });
+
+		Button nonlinefbtn = nonlinef.GetComponent<Button>();
+		ntrainfbtn.onClick.AddListener(delegate() { GoToLevel("boat_online_female"); });
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+	void GoToLevel(string lvl)
+	{
+		// Load the level
+		Application.LoadLevel(lvl);
+	}
+		
+
+
+
+}
